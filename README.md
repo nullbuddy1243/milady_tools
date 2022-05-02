f968961133a81f6a1553f9ba63ee3727a1da65f3
nullbuddy1243 <nullbuddy1243@no.com>
2022-05-02 01:57:37 -0400

    init downloader

---
+++ README.md
@@ -0,0 +1,20 @@
+# milady tools
+
+## download miladys 
+
+simple, mass "right click save as".  
+
+needed all of em for *something* ;) 
+
+``` bash
+cd milady_downloader
+
+python3 - m ./
+
+source ./bin/activate
+
+pip3 install -r requirements.txt 
+
+python3 download.py
+
+```
---
+++ download.py
@@ -0,0 +1,26 @@
+import requests, time, json
+
+# https://www.miladymaker.net/milady/json/6756
+
+base_url = 'https://miladymaker.net/milady/' # tokenId
+token_uri = 'https://www.miladymaker.net/milady/json/' # tokenId
+
+for tokenId in range(0,10000): 
+    img_url = "{}{}.png".format(base_url, tokenId)
+    token_url = "{}{}".format(token_uri, tokenId)
+
+    print("Getting {}\n{}".format(token_url, img_url))
+
+    mil_json = json.loads(requests.get(token_url).content)
+    # print(json.dumps(mil_json))
+
+    with open("./jsons/{}.json".format(tokenId), 'w+') as handler:
+        handler.write(json.dumps(mil_json))
+
+    mil_img = requests.get(img_url).content
+
+    this_mil_png = './img/{}.png'.format(tokenId)
+    with open(this_mil_png, 'wb+') as handler:
+        handler.write(mil_img)
+
+    time.sleep(0.150)
